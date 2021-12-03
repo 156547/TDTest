ad583e57ce84ad2c119d11e10d80672187fe8b5b
isaac <156547@chrome.escg.ac.uk>
2021-12-03 09:50:15 +0000

    merged with files from home

--- DroneController.cs
+++ DroneController.cs
@@ -2,17 +2,30 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class DroneController : MonoBehaviour, IMoveable
+public class DroneController : MonoBehaviour, IMoveable, IDamageable
 {
     [SerializeField] Transform goal;
+    public float droneHealth = 100f;
 
     void Start()
     {
         Move();
     }
+    private void Update()
+    {
+        if(droneHealth <= 0)
+        {
+            Debug.Log("Drone killed");
+            this.gameObject.SetActive(false);
+        }
+    }
     public void Move()
     {
         NavMeshAgent agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
+    }
+    public void Damage(float damageTaken)
+    {
+        droneHealth -= damageTaken;
     }
 }
--- GuardController.cs
+++ GuardController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class GuardController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    #region ATTRIBUTES
+    float time;
+    [SerializeField] float attackSpeed = 2;
+    [SerializeField] float attackDamage = 50f;
+    #endregion
+
+    #region MONOBEHAVIOUR METHODS
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Enemy")
+        {
+            var damageable = other.GetComponent<IDamageable>();
+            damageable.Damage(Attack());
+        }
+    }
+    #endregion
+    #region METHODS
+    float Attack()
+    {
+        time += Time.deltaTime;
+        if (time > attackSpeed)
+        {
+            Debug.Log("Attacking!");
+            time = 0;
+            return attackDamage;
+        }
+        else
+        {
+            return 0f;
+        }
+    }
+    #endregion
 }
