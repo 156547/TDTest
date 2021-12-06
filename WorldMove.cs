9484ac72c1e1cb3e65b719bcfa726814e58fa4d7
isaac <156547@chrome.escg.ac.uk>
2021-12-06 14:52:20 +0000

    added worldmove

---
+++ WorldMove.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMove : MonoBehaviour
+{
+    #region ATTRIBUTES  
+    float timeCount;
+    [SerializeField] float slowTime = 0.02f;
+
+    float currentAngle = 0;
+
+    
+    #endregion
+
+    #region MONOBEHAVIOUR METHODS
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && transform.eulerAngles.x == currentAngle % 360)
+        {
+            currentAngle += 90f;
+            timeCount = 0;
+        }
+        if (transform.eulerAngles.x != currentAngle % 360)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(currentAngle, 0, 0), timeCount);
+            timeCount += Time.deltaTime * slowTime;
+            Debug.Log("sadfasdfasdfafaddfasdfsadfafas");
+
+            Debug.Log(transform.eulerAngles.x);
+            Debug.Log(currentAngle);
+        }
+    }
+    #endregion
+
+    #region METHODS
+
+    #endregion
+}
